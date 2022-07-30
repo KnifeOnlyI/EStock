082baeb3c6fd733d6e994f5c694d3a592049dd29
Dany Pignoux <dany.pignoux@outlook.fr>
2022-07-30 11:22:58 +0200

    Initial commit

---
+++ src/main/java/fr/knife/estockapi/EstockApplication.java
@@ -0,0 +1,13 @@
+package fr.knife.estockapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstockApplication.class, args);
+    }
+
+}
